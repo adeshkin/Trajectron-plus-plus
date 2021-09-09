9dfe1413f69db96077a67f42bf91cf777773859d
adeshkin <adeshkin.vi@phystech.edu>
2021-09-09 16:13:20 +0300

    add train

--- experiments/nuScenes/process_data_scripts/process_data.sh
+++ experiments/nuScenes/process_data_scripts/process_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python process_data.py \
-  --data=/home/adeshkin/Desktop/motion_prediction/nuScenes/v1.0-test \
-  --version="v1.0-test" \
+  --data=/home/adeshkin/Desktop/motion_prediction/nuScenes/v1.0-trainval \
+  --version="v1.0-trainval" \
   --output_path=../../processed
