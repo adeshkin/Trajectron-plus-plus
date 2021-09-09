6679981853bf884540f464e9b6084ea35576030f
adeshkin <adeshkin.vi@phystech.edu>
2021-09-09 16:06:08 +0300

    add mkdir

--- experiments/nuScenes/process_data_scripts/process_data.sh
+++ experiments/nuScenes/process_data_scripts/process_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python process_data.py \
-	--data=/media/cds-k/data/nuScenes/v1.0-mini \
-	--version="v1.0-mini" \
-	--output_path=../../processed
+  --data=/home/adeshkin/Desktop/motion_prediction/nuScenes/v1.0-test \
+  --version="v1.0-test" \
+  --output_path=../../processed
