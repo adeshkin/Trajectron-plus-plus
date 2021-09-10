b4155bf90071634cade4c21505010f77428b9845
adeshkin <adeshkin.vi@phystech.edu>
2021-09-10 12:18:23 +0300

    add previous version derivate_of

--- experiments/nuScenes/process_data_scripts/process_data.sh
+++ experiments/nuScenes/process_data_scripts/process_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python process_data.py \
-  --data=/media/cds-k/data/nuScenes/v1.0-mini \
-  --version="v1.0-mini" \
+  --data=/home/adeshkin/Desktop/motion_prediction/nuScenes/v1.0-trainval \
+  --version="v1.0-trainval" \
   --output_path=../../processed_old
