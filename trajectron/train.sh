6623cd89e98a8543f7febcac0621265a0b71e6ed
adeshkin <adeshkin.vi@phystech.edu>
2021-09-22 10:49:22 +0300

    fix

--- trajectron/train.sh
+++ trajectron/train.sh
@@ -4,7 +4,7 @@ python train.py \
 --eval_every 1 \
 --vis_every 1 \
 --conf ../experiments/nuScenes/models/int_ee_old/config_ph_10_maxhl_4.json \
---data_dir ../experiments/processed_boris \
+--data_dir /media/cds-k/data/nuScenes/traj++_processed_data/processed_boris \
 --train_data_dict nuScenes_train_full.pkl \
 --eval_data_dict nuScenes_val_full.pkl \
 --offline_scene_graph yes \
