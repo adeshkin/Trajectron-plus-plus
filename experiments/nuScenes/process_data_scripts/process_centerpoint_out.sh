7a3aaebbd850e77287d16e8a414b0d0aa6dc8cfe
adeshkin <adeshkin.vi@phystech.edu>
2021-09-28 17:36:31 +0300

    add smth

--- experiments/nuScenes/process_data_scripts/process_centerpoint_out.sh
+++ experiments/nuScenes/process_data_scripts/process_centerpoint_out.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mode='train'
+
 data_dir=/home/cds-k/Desktop/centerpoint_out/centerpoint_out
-python process_centerpoint_out.py \
-  --data_dir=$data_dir \
-  --mode=$mode \
-  --output_path=/media/cds-k/data/nuScenes/traj++_processed_data/processed_centerpoint_out_augment
+for mode in 'train' 'val' 'test'
+do
+  echo $mode
+  python process_centerpoint_out.py \
+    --data_dir=$data_dir \
+    --mode=$mode \
+    --output_path=/media/cds-k/data/nuScenes/traj++_processed_data/processed_centerpoint_out_augment
+done
