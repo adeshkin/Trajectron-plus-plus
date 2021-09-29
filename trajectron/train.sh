4d61ca6ee7ccaeb6f13824df247106714a29de0c
adeshkin <adeshkin.vi@phystech.edu>
2021-09-29 22:06:27 +0300

    update

--- trajectron/train.sh
+++ trajectron/train.sh
@@ -6,12 +6,12 @@ python train.py \
 --conf ../experiments/nuScenes/models/int_ee_old/config_ph_25_maxhl_25.json \
 --data_dir /media/cds-k/data/nuScenes/traj++_processed_data/processed_sdc \
 --train_data_dict sdc_train.pkl \
---eval_data_dict sdc_val.pkl \
---offline_scene_graph yes \
+--eval_data_dict sdc_validation.pkl \
+--offline_scene_graph no \
 --preprocess_workers 10 \
 --batch_size 256 \
 --log_dir ../experiments/nuScenes/models \
---train_epochs 40 \
+--train_epochs 20 \
 --node_freq_mult_train \
 --log_tag _int_ee_sdc_ph_25_maxhl_25 \
 --augment
