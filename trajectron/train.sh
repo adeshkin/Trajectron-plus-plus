d38ac8c986daa0b426f959f798a6cfc3e5d6aac8
adeshkin <adeshkin.vi@phystech.edu>
2021-09-29 17:01:57 +0300

    update

--- trajectron/train.sh
+++ trajectron/train.sh
@@ -3,15 +3,15 @@
 python train.py \
 --eval_every 1 \
 --vis_every 1 \
---conf ../experiments/nuScenes/models/int_ee_old/config_ph_10_maxhl_4.json \
---data_dir /media/cds-k/data/nuScenes/traj++_processed_data/processed_centerpoint_out_augment \
---train_data_dict centerpoint_out_train.pkl \
---eval_data_dict centerpoint_out_val.pkl \
+--conf ../experiments/nuScenes/models/int_ee_old/config_ph_25_maxhl_25.json \
+--data_dir /media/cds-k/data/nuScenes/traj++_processed_data/processed_sdc \
+--train_data_dict sdc_train.pkl \
+--eval_data_dict sdc_val.pkl \
 --offline_scene_graph yes \
 --preprocess_workers 10 \
 --batch_size 256 \
 --log_dir ../experiments/nuScenes/models \
 --train_epochs 40 \
 --node_freq_mult_train \
---log_tag _int_ee_centerpoint_out_ph_10_maxhl_4 \
+--log_tag _int_ee_sdc_ph_25_maxhl_25 \
 --augment
