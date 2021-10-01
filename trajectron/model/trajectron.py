24bdd006011b16feda078569cdda4162f7a8f0ac
adeshkin <adeshkin.vi@phystech.edu>
2021-10-01 08:45:20 +0300

    update

--- trajectron/model/trajectron.py
+++ trajectron/model/trajectron.py
@@ -135,14 +135,10 @@ class Trajectron(object):
                 num_samples=1,
                 min_future_timesteps=0,
                 min_history_timesteps=1,
-                max_ht=None,
                 z_mode=False,
                 gmm_mode=False,
                 full_dist=True,
                 all_z_sep=False):
-
-        if max_ht:
-            self.max_ht = max_ht
 
         predictions_dict = {}
         for node_type in self.env.NodeType:
@@ -190,10 +186,17 @@ class Trajectron(object):
 
             predictions_np = predictions.cpu().detach().numpy()
 
+            mask_agents = np.array([repr(node).split('/')[1] in scene.prediction_request_agent_ids for node in nodes])
+            if True not in mask_agents:
+                continue
+
+            predictions_np_pr = predictions_np[:, mask_agents]
+            timesteps_o_pr = np.array(timesteps_o)[mask_agents]
+            nodes_pr = np.array(nodes)[mask_agents]
             # Assign predictions to node
-            for i, ts in enumerate(timesteps_o):
+            for i, ts in enumerate(timesteps_o_pr):
                 if ts not in predictions_dict.keys():
                     predictions_dict[ts] = dict()
-                predictions_dict[ts][nodes[i]] = np.transpose(predictions_np[:, [i]], (1, 0, 2, 3))
+                predictions_dict[ts][nodes_pr[i]] = np.transpose(predictions_np_pr[:, [i]], (1, 0, 2, 3))
 
         return predictions_dict
