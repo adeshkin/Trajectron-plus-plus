8635003b1d2793c4d31088fddce3e0d1a6ffdbe4
adeshkin <adeshkin.vi@phystech.edu>
2021-10-05 10:32:00 +0300

    update

--- trajectron/train.sh
+++ trajectron/train.sh
@@ -3,8 +3,7 @@
 python train.py \
 --conf ../experiments/nuScenes/train_configs/config_ph_25_maxhl_24_minhl_24.json \
 --preprocess_workers 10 \
---batch_size 256 \
+--batch_size 64 \
 --log_dir ../experiments/nuScenes/models \
 --train_epochs 50 \
---node_freq_mult_train \
---log_tag _int_ee_sdc_ph_25_maxhl_24_min_hl_24_bs_256
+--log_tag _int_ee_sdc_ph_25_maxhl_24_min_hl_24_bs_64
